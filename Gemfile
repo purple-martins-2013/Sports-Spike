b19f331e62f2e5a64ebe852935266f5e5eda6d6a
Apprentice <apprentice@dbc21.local>
2013-09-05 12:28:59 -0700

    add tweetstream gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 group :test, :development do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'tweetstream'
 end
 
 group :test do
---
+++ config/initializers/tweetstream_initializer.rb
@@ -0,0 +1,9 @@
+require 'tweetstream'
+
+TweetStream.configure do |config|
+  config.consumer_key         = 'EJboNtzlFa0wX7FNjZtlg'  
+  config.consumer_secret      = 'z1n9xc4T8BsLPmnT6bTnTKS75ZmFXm01kpqrzIV4ho'
+  config.oauth_token          = '1732712186-mzZtp0RcXDKFOKUyek7fv3W1R3x2EuJqPKBF9Dn'
+  config.oauth_token_secret   = 'oksEY9j5FVrSZc9vWDxr7fdsY9sKsT91TyqKhHV7GxQ'
+  config.auth_method          = :oauth
+end
