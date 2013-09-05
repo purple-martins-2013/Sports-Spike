2b9fd8660989ff12dc2a20bd1ec2cce981bfa8be
Apprentice <apprentice@devbootcamp.com>
2013-09-05 12:03:45 -0700

    valid factory

--- spec/factories/spikes.rb
+++ spec/factories/spikes.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :spike do
+    sequence(:date_time) { |n| Time.now + n * 3600 }
+    peak_velocity { rand(100) * 10 }
   end
 end
--- spec/models/spike_spec.rb
+++ spec/models/spike_spec.rb
@@ -5,4 +5,13 @@ describe Spike do
   it { should validate_presence_of(:peak_velocity)}
   it { should validate_presence_of(:date_time)}
 
+  describe 'with valid information' do
+    let(:spike) { create :spike }
+
+    it 'should be valid' do
+      expect(spike).to be_valid
+    end
+
+  end
+
 end
