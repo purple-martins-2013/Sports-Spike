4b6acbcd383a5625e0629ec7765ceceb7802f53f
Steven Harlow <STHarlow@gmail.com>
2013-09-06 12:50:51 -0700

    fixes for heroku

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,5 @@ class TweetsController < ApplicationController
     store = TweetStore.new
     @redis_trip = RedisTrip.all.reverse_order
     @tweets = store.tweets
-    @time = RedisTrip.time_between_last_two_trips
   end
 end
