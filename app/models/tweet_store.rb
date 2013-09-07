84a8d1d878018aafa3bea2d006bcddd92e61ee1d
Apprentice <apprentice@dbc21.local>
2013-09-07 14:58:41 -0700

    remove hardcoded variables from tweet_store.rb

--- app/models/tweet_store.rb
+++ app/models/tweet_store.rb
@@ -6,31 +6,33 @@ class TweetStore
   attr_reader :tweet_count
 
   REDIS_KEY = 'redis_tweets'
-  # NUM_TWEETS = 10000
-  CACHE_THRESHOLD = 60
 
   def initialize
     @db = Redis.new
     @tweet_count = 0
     @start_time = Time.new
+    @interval = 60
   end
 
-  # def tweets(limit=10, since=0)
-  #   @db.LRANGE(REDIS_KEY, 0, limit -1).collect {|t|
-  #       (JSON.parse(t))
-  #     }
-  # end
+  def count_reset
+    @tweet_count = 0
+  end
+
+  def inc_tweet_count
+    @tweet_count += 1
+  end
+
+  def time_reset
+    @start_time = Time.new
+  end
 
   def push(data)
     @db.LPUSH(REDIS_KEY, data.to_json)
-    @tweet_count += 1
-    
-    if (Time.now - @start_time >= 60)
+    inc_tweet_count
+    if (Time.now - @start_time >= @interval)
       RedisTrip.create(time: Time.now, tweet_count: @tweet_count)
-      # p RedisTrip.test
-      # @db.LTRIM(REDIS_KEY, 0, NUM_TWEETS)
-      @tweet_count = 0
-      @start_time = Time.new
+      count_reset
+      time_reset
     end
      p @tweet_count
   end
