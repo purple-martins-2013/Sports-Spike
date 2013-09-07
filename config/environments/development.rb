9436f6d5299f5e68eaca01ec09f093e012ba0289
mehagel65@gmail.com <mehagel65@gmail.com>
2013-09-06 17:07:05 -0700

    REDIS TO GO changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ SportsSpike::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
+ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389' 
 end
