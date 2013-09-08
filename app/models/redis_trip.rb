dbd1f92fb1558eefed4944c0124d35714ef2fcc5
Apprentice <apprentice@devbootcamp.com>
2013-09-07 17:11:47 -0700

    flesh out algorithm logic

--- app/models/redis_trip.rb
+++ app/models/redis_trip.rb
@@ -1,25 +1,46 @@
 class RedisTrip < ActiveRecord::Base
+  SIGNAL_LINE_PERIOD = 3
+  SHORT_EMA_PERIOD = 5
+  LONG_EMA_PERIOD = 10
  
   validates :time, presence: true
 
   before_create :populate_algorithm_fields
 
+  private
+
+  # TODO: CLEAN UP THIS UGLY CODE~!!!!!!!
+
   def populate_algorithm_fields
-    
+    calculate_short_ema if RedisTrip.all.count > SHORT_EMA_PERIOD
+    calculate_long_ema if RedisTrip.all.count > LONG_EMA_PERIOD
+    calculate_macd if RedisTrip.all.count > LONG_EMA_PERIOD
+    calculate_signal_line if RedisTrip > LONG_EMA_PERIOD + SIGNAL_LINE_PERIOD
   end
 
   def calculate_short_ema
-    sc = smoothing_constant(n)
-    RedisTrip.order_by()
+    self.short_ema = RedisTrip.all.pluck('tweet_count').inject(:+) / SHORT_EMA_PERIOD if RedisTrip.all.size == SHORT_EMA_PERIOD + 1
+    sc = smoothing_constant(SHORT_EMA_PERIOD)
+    last_ema = RedisTrip.order('created_at DESC').first.short_ema
+    self.short_ema = last_ema + sc * ( self.tweet_count - last_ema )
   end
 
   def calculate_long_ema
+    self.long_ema = RedisTrip.all.pluck('tweet_count').inject(:+) / LONG_EMA_PERIOD if RedisTrip.all.size == SHORT_EMA_PERIOD + 1
+    sc = smoothing_constant(LONG_EMA_PERIOD)
+    last_ema = RedisTrip.order('created_at DESC').first.long_ema
+    self.long_ema = last_ema + sc * ( self.tweet_count - last_ema )
   end
 
   def calculate_macd
+    self.macd = self.short_ema - self.long_ema
   end
 
   def calculate_signal_line
+    self.signal_line = RedisTrip.order('created_at DESC').limit(3).pluck('macd').inject(:+) / SIGNAL_LINE_PERIOD if RedisTrip.all.size == SIGNAL_LINE_PERIOD + 1
+    sc = smoothing_constant(SIGNAL_LINE_PERIOD)
+    last_signal_line = RedisTrip.order('created_at DESC').first.signal_line
+    self.signal_line = last_signal_line + sc * ( self.macd - last_signal_line )
   end
 
   def smoothing_constant(n)  #extract to helper
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907224853) do
+ActiveRecord::Schema.define(version: 20130907231233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20130907224853) do
     t.datetime "time"
     t.integer  "short_ema"
     t.integer  "long_ema"
-    t.integer  "macd_ema"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "macd"
+    t.integer  "signal_line"
   end
 
   create_table "spikes", force: true do |t|
