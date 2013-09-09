a635f2c67d3eb074c9c666c03a278e44e261bf9c
Apprentice <apprentice@dbc21.local>
2013-09-09 14:41:27 -0700

    with initial hard-coded chart.js object

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
  
   def index
     store = TweetStore.new
-    @redis_trip = RedisTrip.all.reverse_order
+    @redis_trip = RedisTrip.all
+    
   end
 end
