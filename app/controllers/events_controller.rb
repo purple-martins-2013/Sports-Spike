a85c5ff23165c7cb9d93019a93a364abe5ba55d7
Apprentice <apprentice@dbc21.local>
2013-09-10 10:14:00 -0700

    remove app.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,5 @@
 class EventsController < ApplicationController
-
-  def app
-  end
-
   def index
     @events = Event.all
-    render json: @events.to_json(:include => {:search_terms => {:include => :redis_trips}})
   end
 end
