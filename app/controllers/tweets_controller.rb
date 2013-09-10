504d057ae0c6fd3357c5de614ef80d4494a27811
Apprentice <apprentice@dbc21.local>
2013-09-10 10:22:03 -0700

    remove tweets controller

--- app/controllers/tweets_controller.rb
+++
@@ -1,7 +0,0 @@
-class TweetsController < ApplicationController
-  def index
-    store = TweetStore.new
-    @redis_trip = RedisTrip.all.reverse_order
-    @tweets = store.tweets
-    @espn=ESPN.get_nfl_scores(2013, 1)
-end
