dc6f6087b710f4a4da9a382c2655dc66bc88cd0e
Apprentice <apprentice@devbootcamp.com>
2013-09-10 16:12:10 -0700

    add zoom function to 1 and 4hr, 1day times. Include scroll bar and navigator bar

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -44,6 +44,60 @@ var Chart = {
 
       },
 
+      rangeSelector: {
+        enabled: true,
+        inputEnabled: false,
+        buttons: [{
+            type: 'hour',
+            count: 1,
+            text: '1hr'
+        },{
+            type: 'hour',
+            count: 4,
+            text: '4hr'
+        },{
+            type: 'day',
+            count: 1,
+            text: 'day'
+        }
+        ],
+        buttonTheme: { // styles for the buttons
+          fill: 'none',
+          stroke: 'none',
+          'stroke-width': 0,
+          r: 8,
+          style: {
+            color: '#039',
+            fontWeight: 'bold'
+          },
+          states: {
+            hover: {
+            },
+            select: {
+              fill: '#039',
+              style: {
+                color: 'white'
+              }
+            }
+          }
+        },
+        inputStyle: {
+          color: '#039',
+          fontWeight: 'bold'
+        },
+        labelStyle: {
+          color: 'silver',
+          fontWeight: 'bold'
+        },
+        selected: 1
+      },
+      
+
+      scrollbar: {
+        enabled: true,
+        liveRedraw: true
+      },
+
       legend: {
         enabled: false
       },
--- app/controllers/redis_trips_controller.rb
+++ app/controllers/redis_trips_controller.rb
@@ -9,7 +9,8 @@ class RedisTripsController < ApplicationController
       formatted_date = trip.created_at.strftime("%Y-%m-%d %I:%M")
       [formatted_date, trip.tweet_count]
     end
-  
+    puts ('#') * 50
+    p data
     render :json => data.sort_by { |d| DateTime.parse(d[0]) }
   end
 end
