5e772ea2315d6a071864dc482f9f9ebaf047207a
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-10 10:44:54 -0700

    initial refactor of mvc pattern

--- app/controllers/redis_trips_controller.rb
+++ app/controllers/redis_trips_controller.rb
@@ -1,9 +1,20 @@
 class RedisTripsController < ApplicationController
  
   def index
-    store = TweetStore.new
-    @redis_trip = RedisTrip.all.reverse_order
-    @tweets = store.tweets
+    @teams = SearchTerm.all
   end
+
+  def team_pulse
+    @trip = RedisTrip.where('search_term_id = ')
+  end
+
+  # def index
+  #   @redis_trip_display = RedisTrip.all
+  #   @chart_array = []
+  #   created_at = RedisTrip.where('search_term_id = 15').pluck('created_at')
+  #   tweet_count = RedisTrip.where('search_term_id = 15').pluck('tweet_count')
+  #   @chart_array << created_at.zip(tweet_count)
+  #   render json: @chart_array
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SportsSpike::Application.routes.draw do
 
-  root 'events#app'
+  root 'redis_trips#index'
   resources :events, only: [:index]
   resources :redis_trips, only: [:index]
+
+  get 'redis_trips/:search_term_id' => 'redis_trips#team_pulse'
 
 end
