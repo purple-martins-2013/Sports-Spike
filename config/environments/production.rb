ca50387503d714633c534337eae9bc3447df17a8
Steven Harlow <STHarlow@gmail.com>
2013-09-10 15:18:03 -0700

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ SportsSpike::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( chart.js chart.min.js spikes.js foundation_and_overrides.scss home.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
