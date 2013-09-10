6f9f4ab5a60d417e71236ced9995a0fc72f7d26c
mehagel65@gmail.com <mehagel65@gmail.com>
2013-09-10 16:10:29 -0700

    hashtag logic

--- app/models/spike.rb
+++ app/models/spike.rb
@@ -5,5 +5,11 @@ class Spike < ActiveRecord::Base
   def time_since_game_start
     self.date_time.time - self.event.date.time
   end
+
+ def find_hashtag(spike_event_id)
+ 	redis_trip_id = Spike.find(spike_event_id).reduis_trip_id
+ 	search_term_id = RedisTrip.find(@redis_trip_id).search_term_id
+ 	@hashtag = SearchTerm.find(search_term_id).hashtag
+ end
   
 end
