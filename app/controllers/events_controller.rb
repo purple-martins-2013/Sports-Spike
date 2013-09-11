8fee3dfe1832eec3bd3d1bfdb40cc9077cf612e0
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-10 17:24:29 -0700

    begin setup for controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,9 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+    # @trips = RedisTrip.where()
+  end
 end
