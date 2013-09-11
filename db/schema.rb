82172c85a86e6cc2dc950866ac2e10a5458b0dba
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-11 16:06:28 -0700

    a spike should have the right game status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909225006) do
+ActiveRecord::Schema.define(version: 20130911210759) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -43,6 +46,7 @@ ActiveRecord::Schema.define(version: 20130909225006) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "redis_trip_id"
+    t.string   "game_status"
   end
 
 end
--- spec/models/spike_spec.rb
+++ spec/models/spike_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe Spike do
 
+  let(:term) { create :search_term, id: 28, hashtag: "#49ers" }
+  let(:trip) { create :redis_trip, search_term: term }
+  let(:spike) { create :spike, redis_trip: trip }
+
   it { should belong_to(:redis_trip) }
 
   describe 'with valid information' do
-    let(:spike) { create :spike }
 
     it 'should be valid' do
       expect(spike).to be_valid
+    end
+
+    it 'should have an associated search_term' do 
+      expect(spike.redis_trip.search_term.hashtag).to eq "#49ers"
+    end
+
+    it 'should have a game status' do
+      expect(spike.game_status).to_not be_nil
     end
   end
 end
