5e6d1f520e99925b52785a178b1f7e4f04cce6af
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-11 16:53:34 -0700

    remove unnecessary methods from RedisTripsController

--- app/controllers/redis_trips_controller.rb
+++ app/controllers/redis_trips_controller.rb
@@ -1,18 +1,6 @@
 class RedisTripsController < ApplicationController
-  
-
   def index
     @trips = RedisTrip.all
     @teams = SearchTerm.all
-  end
-  
-  def teams
-    
-  end
-
-
-  def team_pulse
-    # @trips = RedisTrip.where(search_term_id: params[:search_term_id])
-    # @team_name = @trips.first.search_term.hashtag
   end
 end
