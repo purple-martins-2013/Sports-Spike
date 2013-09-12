af0f89904e0b9c7f65f5049d8df1b79dd3ee7b7e
Steven Harlow <STHarlow@gmail.com>
2013-09-11 18:16:53 -0700

    change tweetstream to env variables

--- config/initializers/tweetstream_initializer.rb
+++ config/initializers/tweetstream_initializer.rb
@@ -1,9 +1,9 @@
 require 'tweetstream'
 
 TweetStream.configure do |config|
-  config.consumer_key         = TWEETSTREAM_CONSUMER_KEY  
-  config.consumer_secret      = TWEETSTREAM_CONSUMER_SECRET
-  config.oauth_token          = TWEETSTREAM_OAUTH_TOKEN
-  config.oauth_token_secret   = TWEETSTREAM_OAUTH_SECRET
+  config.consumer_key         = ENV['TWEETSTREAM_CONSUMER_KEY']
+  config.consumer_secret      = ENV['TWEETSTREAM_CONSUMER_SECRET']
+  config.oauth_token          = ENV['TWEETSTREAM_OAUTH_TOKEN']
+  config.oauth_token_secret   = ENV['TWEETSTREAM_OAUTH_SECRET']
   config.auth_method          = :oauth
 end
