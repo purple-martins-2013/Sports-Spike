a314589171830de06f5f13fbf366a9b518dd0d1a
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-12 12:36:46 -0700

    charts are loaded for two teams"

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,136 +1,298 @@
-Highcharts.setOptions({
-      global: {
-        useUTC: false
-      }
-});
+function makeTweetGraph() {
+  var myChart = {
 
-var MyChart = {
+    pollTime: 60000,
 
-  pollTime: 60000,
+    init: function() {
+      this.render();
+      this.poll();
+      
+    },
 
-  init: function() {
-    this.render();
-    this.poll();
-    
-  },
+    poll: function() {
+      setInterval(this.render, this.pollTime)
+    },
 
-  poll: function() {
-    setInterval(this.render, this.pollTime)
-  },
-
-  render: function() {
-    var pathname = window.location.pathname;
-    var id = pathname[pathname.length - 1];
-    console.log(id);
-    $.get('/search_terms/' + id, function(data) {
-      var newData = [];
-      var data = $.each(data, function(k, coordinate) {
-        newData.push([Date.parse(coordinate[0]), coordinate[1]]);
+    render: function() {
+      $.get('/team_pulse/' + teamOneId + '/' + teamTwoId, function(stats) {
+        console.log(stats)
+        var teamOne = [];
+        $.each(stats.tweets_by_team_one, function(k, coordinate) {
+          teamOne.push([Date.parse(coordinate[0]), coordinate[1]]);
+        });
+        var teamTwo = [];
+        $.each(stats.tweets_by_team_two, function(k, coordinate) {
+          teamTwo.push([Date.parse(coordinate[0]), coordinate[1]]);
+        });
+        myChart.draw(teamOne, teamTwo);
       });
-      MyChart.draw(newData);
-    });
-  },
+    },
 
-  draw: function(data) {
-    new Highcharts.Chart({
-      chart: {
-        renderTo: 'chart'
-      },
-      title: {
-        text: null
-      },
-      navigator: {
-        enabled: true,
-        adaptToUpdatedData: false
+    draw: function(data1, data2) {
+      new Highcharts.Chart({
+        chart: {
+          renderTo: 'chart',
+          type: 'areaspline',
+          defaultSeriesType: 'spline'
+        },
+        title: {
+          text: teamOneName + ' vs ' + teamTwoName
+        },
+        navigator: {
+          enabled: true,
+          adaptToUpdatedData: true
 
-      },
+        },
 
-      rangeSelector: {
-        enabled: true,
-        inputEnabled: false,
-        buttons: [{
-            type: 'all',
-            text: 'All'
-        },{
-            type: 'hour',
-            count: 1,
-            text: '1hr'
-        },{
-            type: 'hour',
-            count: 4,
-            text: '4hr'
-        },{
-            type: 'day',
-            count: 1,
-            text: '1 day'
-        }
-        ],
-        buttonTheme: { // styles for the buttons
-          fill: 'none',
-          stroke: 'none',
-          'stroke-width': 0,
-          r: 8,
-          style: {
+        rangeSelector: {
+          enabled: true,
+          inputEnabled: false,
+          buttonSpacing: 1,
+          buttons: [{
+              type: 'all',
+              text: 'All'
+          },{
+              type: 'hour',
+              count: 1,
+              text: '1hr'
+          },{
+              type: 'hour',
+              count: 4,
+              text: '4hr'
+          },{
+              type: 'day',
+              count: 1,
+              text: 'Day'
+          }
+          ],
+          buttonTheme: { // styles for the buttons
+            stroke: 'none',
+            'stroke-width': 0,
+            r: 8,
+            style: {
+              color: '#039',
+              fontWeight: 'bold'
+            },
+            states: {
+              hover: {
+              },
+              select: {
+                fill: '#039',
+                style: {
+                  color: 'white'
+                }
+              }
+            }
+          },
+          inputStyle: {
             color: '#039',
             fontWeight: 'bold'
           },
-          states: {
-            hover: {
-            },
-            select: {
-              fill: '#039',
-              style: {
-                color: 'white'
+          labelStyle: {
+            color: 'silver',
+            fontWeight: 'bold'
+          },
+
+        },
+        
+        plotOptions: {
+          series: {
+            marker: {
+              enabled: false,
+              states: {
+                hover: {
+                  enabled: true
+                }
               }
             }
           }
         },
-        inputStyle: {
-          color: '#039',
-          fontWeight: 'bold'
-        },
-        labelStyle: {
-          color: 'silver',
-          fontWeight: 'bold'
+        scrollbar: {
+          enabled: true,
+          liveRedraw: true
         },
 
-      },
-      
+        legend: {
+          enabled: true,
+        },
 
-      scrollbar: {
-        enabled: true,
-        liveRedraw: true
-      },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+              minute: '%d<br/>%H:%M',
+              hour: '%d<br/>%H:%M',
+              day: '%m-%d',
+              week: '%m-%d',
+            },
+            minRange: 180 * 1000
+        },
 
-      legend: {
-        enabled: false
-      },
-      plotOptions: {
-        cropThreshold:10000
-      },
-      xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: {
-            minute: '%d<br/>%H:%M',
-            hour: '%d<br/>%H:%M',
-            day: '%m-%d',
-            week: '%m-%d',
-          },
-          minRange: 180 * 1000
-      },
+        yAxis: {
+          enabled: true,
+          min: 0,
 
-      yAxis: {
-        enabled: false
-      },
+        },
 
-      series: [{
-  
-          name: 'Tweets Per Minute',
-          data: data,
-          dataGrouping: {
-            enabled: false
-          }
-        }]
-    });
+        series: [{
+    
+            name: 'Team One',
+            data: data1,
+            threshold: null,
+            dataGrouping: {
+              enabled: true
+            },
+            },{
+            name: 'Team Two',
+            data: data2,
+            threshold: null,
+            dataGrouping: {
+              enabled: true
+            }
+          }]
+      });
+    }
   }
+  
+  return myChart
+}
+
+
+function makePulseChart() {
+ var pulseChart = {
+
+    pulseTime: 60000,
+
+    init: function() {
+      this.render();
+      this.poll();
+      
+    },
+
+    poll: function() {
+      setInterval(this.render, this.pulseTime)
+    },
+
+    render: function() {
+      $.get('/team_pulse/' + teamOneId + '/' + teamTwoId, function(stats) {
+        var pulseDataTeamOne = stats.fan_pulse_team_one;
+        var pulseDataTeamTwo = stats.fan_pulse_team_two;
+        pulseChart.draw(pulseDataTeamOne, pulseDataTeamTwo);
+      });
+    },
+
+    draw: function(data1, data2) {
+    //   if (!pulseChart.chart) {
+    //     pulseChart.createChart()
+    //   }
+    //   else 
+    //   chart.series[0].data[0].update.(data1);
+    //   chart.series[1].data[0].update.(data2);
+    //   chart.redraw();
+    // },
+    // createChart: function(data1, data2) {
+    //   this.chart = new Highcharts.Chart({
+        new Highcharts.Chart({
+        chart: {
+            renderTo: 'container',
+            type: 'gauge',
+            plotBorderWidth: 1,
+            plotBackgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF4C6'],
+                    [0.3, '#FFFFFF'],
+                    [1, '#FFF4C6']
+                ]
+            },
+            plotBackgroundImage: null,
+            height: 200
+        },
+    
+        title: {
+            text: 'Fan Excitement'
+        },
+        
+        pane: [{
+            startAngle: -45,
+            endAngle: 45,
+            background: null,
+            center: ['25%', '145%'],
+            size: 300
+          },{
+            startAngle: -45,
+            endAngle: 45,
+            background: null,
+            center: ['75%', '145%'],
+            size: 300
+        }],
+        yAxis: [{
+            min: 0,
+            max: 15,
+            minorTickPosition: 'outside',
+            tickPosition: 'outside',
+            tickInterval: 5,
+            labels: {
+                rotation: 'auto',
+                distance: 20
+            },
+            plotBands: [{
+                from: 12,
+                to: 15,
+                color: '#C02316',
+                innerRadius: '100%',
+                outerRadius: '105%'
+            }],
+            pane: 0,
+            title: {
+                text: 'Team One',
+                y: -40
+            }
+        }, {
+            min: 0,
+            max: 15,
+            minorTickPosition: 'outside',
+            tickPosition: 'outside',
+            tickInterval: 5,
+            labels: {
+                rotation: 'auto',
+                distance: 20
+            },
+            plotBands: [{
+                from: 12,
+                to: 15,
+                color: '#C02316',
+                innerRadius: '100%',
+                outerRadius: '105%'
+            }],
+            pane: 1,
+            title: {
+                text: 'Team Two',
+                y: -40
+            }
+        }],
+
+        plotOptions: {
+          gauge: {
+            dataLabels: {
+              enable: true
+            },
+          },
+          dial: {
+            radius: '100%'
+          }
+        },
+        series: [{
+            data: data1,
+            name: teamOneName,
+            yAxis: 0
+            }, {
+            data: data2,
+            name: teamTwoName,
+            yAxis: 1
+        }]
+
+      });
+    }
+  }
+  return pulseChart
 }
--- app/controllers/redis_trips_controller.rb
+++ app/controllers/redis_trips_controller.rb
@@ -3,4 +3,12 @@ class RedisTripsController < ApplicationController
     @trips = RedisTrip.all
     @teams = SearchTerm.all
   end
+
+  def teams
+    team_one = SearchTerm.find_by_hashtag(params[:team_one])
+    p team_one
+    team_two = SearchTerm.find_by_hashtag(params[:team_two])
+    p team_two
+    redirect_to search_term_path(team_one, team_two)
+  end
 end
